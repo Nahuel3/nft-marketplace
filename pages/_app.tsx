1fd92cfc205e9fa36fca1068aeddeb726d9fbf9c
Nahuel <nahuel.valentin@hotmail.com>
2022-09-24 01:20:30 -0300

    doble smart contract

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {BaseLayout, Navbar} from "../components/ui"
-import { Web3Provider } from '@providers'
-import { Web3Providers } from '@providers'
+import { Web3Provider } from '@providers' //FOXY
+import { Web3Providers } from '@providers' //ALUFI
 
 
 function MyApp({ Component, pageProps }: AppProps) {
